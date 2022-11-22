586c8c319c7db3a49e28523abb3299b927ae391b
chalanzi <275528660@qq.com>
2022-11-23 00:16:41 +0800

    增加测试

--- abi/src/lib.rs
+++ abi/src/lib.rs
@@ -1,13 +1,9 @@
+mod error;
 mod pb;
+mod types;
+mod utils;
 
-use chrono::{DateTime, NaiveDateTime, Utc};
+pub use error::{Error, ReservationConflictInfo, ReservationWindow};
 pub use pb::*;
-use prost_types::Timestamp;
-
-pub fn convert_to_utc_time(ts: Timestamp) -> DateTime<Utc> {
-    DateTime::<Utc>::from_utc(
-        NaiveDateTime::from_timestamp_opt(ts.seconds, ts.nanos as _)
-            .expect("invalid or out-of-range datetime"),
-        Utc,
-    )
-}
+pub use types::*;
+pub use utils::*;
